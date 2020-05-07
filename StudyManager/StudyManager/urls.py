6e0631c8f3d35feedb247f6f7fb591a383ed7f70
Johannes W <walterjohannes@gmx.at>
2020-05-07 11:30:12 +0200

    Registration Started

--- StudyManager/StudyManager/urls.py
+++ StudyManager/StudyManager/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
+from registration import views as registration_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('registration/', signUp)
+    path('registration/', registration_views.signup, name="signup"),
 ]
